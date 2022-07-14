54e265ff0e8c5bd7f7320e85a4e428ca10695b45
Siar Wahidi <siar.wahidi@gmail.com>
2022-07-14 16:26:08 -0400

    added react bootstrap form to add new task

--- src/components/FormModal/FormModal.jsx
+++ src/components/FormModal/FormModal.jsx
@@ -1,6 +1,9 @@
 import "./FormModal.scss";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+
 import { Modal } from "react-bootstrap";
 
 const FormModal = ({ isOpen }) => {
@@ -12,7 +15,26 @@ const FormModal = ({ isOpen }) => {
       aria-labelledby="contained-modal-title-vcenter"
       centered
     >
-      <Modal.Body>{/* Need to place a form in here */}</Modal.Body>
+      <div className="modal__wrapper">
+        <Modal.Title className="modal__title">New Task</Modal.Title>
+        <Modal.Body className="modal__body">
+          <Form>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Title</Form.Label>
+              <Form.Control type="title" placeholder="Task Title" />
+            </Form.Group>
+
+            <Form.Group className="mb-3">
+              <Form.Label>Summary</Form.Label>
+              <Form.Control type="password" placeholder="Task Summary" />
+            </Form.Group>
+
+            <Button variant="primary" type="submit">
+              Create New Task
+            </Button>
+          </Form>
+        </Modal.Body>
+      </div>
     </Modal>
   );
 };
